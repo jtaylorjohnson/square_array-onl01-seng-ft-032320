33e2acafa31770ec70dde6fcb382ba8e8f665eca
bustling-clone-9138 <jtaylorjohnson@gmail.com>
2020-03-25 20:08:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,8 @@
 numbers = [1, 2, 3]
-new_array = []
 def square_array(numbers)
   new_array = []
   numbers.each do |squared|
-  new = squared**2
-  new_array << new 
+  new_array << squared**2 
 end
 end
 puts new_array
